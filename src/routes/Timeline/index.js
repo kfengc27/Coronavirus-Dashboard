8bc6f500efb1ebf8154b4436d65f0caa2ea0c8b6
Gerrit Alex <admin@gerritalex.de>
2020-02-02 20:48:43 +0100

    add timeline route

--- src/routes/Timeline/index.js
+++ src/routes/Timeline/index.js
@@ -1,5 +1,41 @@
-import React from 'react';
+import { faBroadcastTower } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState, useEffect } from 'react';
+import { Card, CardFooter, CardBody } from 'reactstrap';
+
+import { ExternalLink } from '../../components';
+
+const dummyData = require('./api.json');
 
 export default function Timeline() {
-  return <h1>timeline</h1>;
+  const [data, setData] = useState(dummyData);
+
+  // useEffect to load data
+
+  return (
+    <section>
+      <h1>Coronavirus Timeline (GMT)</h1>
+      {data.map(({ date, source, text }) => {
+        const { host } = new URL(source);
+
+        return (
+          <article key={source}>
+            <Card>
+              <CardBody className="font-weight-bold text-white">
+                <h5 className="mb-0">
+                  <FontAwesomeIcon icon={faBroadcastTower} color="#F65164" />{' '}
+                  {text}
+                </h5>
+              </CardBody>
+              <CardFooter>
+                {/* todo: fix datetime */}
+                <time dateTime={date}>{date}</time> via{' '}
+                <ExternalLink href={source} text={host} />
+              </CardFooter>
+            </Card>
+          </article>
+        );
+      })}
+    </section>
+  );
 }
