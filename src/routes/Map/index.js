adbb98c66a030977d0646c593be243074f02693c
Gerrit Alex <admin@gerritalex.de>
2020-02-03 00:27:18 +0100

    added Map route

--- src/routes/Map/index.js
+++ src/routes/Map/index.js
@@ -1,5 +1,22 @@
+import { faMap } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
+import styles from './Map.module.scss';
+
 export default function Map() {
-  return <h1>map</h1>;
+  return (
+    <section>
+      <h1>
+        <FontAwesomeIcon icon={faMap} /> Map
+      </h1>
+      <article>
+        <iframe
+          className={styles.frame}
+          src="https://maphub.net/embed/81509?legend=1&panel=1"
+          title="maphub"
+        />
+      </article>
+    </section>
+  );
 }
